48f394e41ce011fb2feb0d614c43619c2052e72c
HariniRamanam <harini.honey101@gmail.com>
2020-09-13 21:43:17 +0530

    First commit.

---
+++ FlowersAR/Assets/FlowerMang.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerMang : MonoBehaviour
+{
+    public GameObject f1, f2, f3;
+
+    // Start is called before the first frame update
+    public void F1()
+    {
+        f1.SetActive(true);
+        f2.SetActive(false);
+        f3.SetActive(false);
+    }
+    public void F2()
+    {
+        f1.SetActive(false);
+        f2.SetActive(true);
+        f3.SetActive(false);
+    }
+    public void F3()
+    {
+        f1.SetActive(false);
+        f2.SetActive(false);
+        f3.SetActive(true);
+    }
+}
